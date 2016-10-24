326b95eee31fa2883b2b563f295d44db0d598495
Kirill Kravinsky <woyorus@gmail.com>
2016-10-24 15:10:46 -0400

    Dumb prototype impl

--- index.js
+++ index.js
@@ -1,8 +1,94 @@
-module.exports = {
-  createPublisher: function(options) {
-    throw "unimplmemented"
-  },
-  createSubscriber: function(options) {
-    throw "unimplemented";
+const amqp = require('amqplib/callback_api');
+const events = require('events');
+
+class Publisher {
+  ch: any;
+
+  constructor(amqpHost: string) {
+    super();
+    amqp.connect(`amqp://${amqpHost}`, function (err, conn) {
+      if (err) {
+        throw new Error("cannot connect to AMQP server: " + err);
+      }
+
+      conn.createChannel(function (err, ch) {
+        this.ch = ch;
+      })
+    })
   }
+
+  publish(exchange: string, message: string): void {
+    this.ch.assertExchange(exchange, 'fanout', { durable: false });
+    this.ch.publish(exchange, '', new Buffer(message));
+  }
+}
+
+class Subscriber extends events.EventEmitter {
+  connection: any;
+  ch: any;
+  q: any;
+  emitter: any;
+  subscriptions: any;
+
+  constructor(amqpHost: string) {
+    super();
+    this.emitter = new events.EventEmitter();
+    this.subscriptions = new Map();
+    amqp.connect(`amqp://${amqpHost}`, function (err, conn) {
+      if (err) {
+        throw new Error("cannot connect to AMQP server: " + err);
+      }
+
+      this.connection = conn;
+    })
+  }
+
+  subscribe(exchange: string): void {
+    this.connection.createChannel(function (err, ch) {
+      if (err) {
+        throw new Error("cannot create AMQP channel: " + err);
+      }
+
+      this.ch = ch;
+      ch.assertExchange(exchange, 'fanout', { durable: false });
+      ch.assertQueue('', { exclusive: true }, function (err, q) {
+        if (err) {
+          throw new Error("failed to assert queue: " + err)
+        }
+
+        this.q = q;
+
+        let handler = (message) => {
+          this.emit('message', exchange, message.content.toString());
+        }
+
+        this.subscriptions.set(exchange, handler);
+        ch.bindQueue(q.queue, exchange, '');
+        ch.consume(q.queue, handler, { noAck: true })
+      })
+    })
+  }
+
+  unsubscribe(exchange: string): void {
+    if (!this.subscriptions.has(exchange)) {
+      return;
+    }
+
+    this.emitter.removeListener(exchange, this.subscriptions.get(exchange));
+    this.subscriptions.delete(exchange);
+    this.ch.unbindQueue(this.q.queue, exchange, '');
+  }
+}
+
+let AMQPPubSub = {
+  createPublisher: function (opts): any {
+    return new Publisher(opts.amqpHost)
+  },
+  createSubscriber: function (opts) {
+    return new Subscriber(opts.amqpHost)
+  }
+}
+
+export {
+  AMQPPubSub
 }
